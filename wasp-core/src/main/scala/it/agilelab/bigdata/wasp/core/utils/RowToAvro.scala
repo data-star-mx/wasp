8f5f3b7b9b50dd60e0acb115ccdcf101e66f196a
Nicolò Bidotti <nicolo.bidotti@gmail.com>
2017-02-08 16:56:23 +0100

    Add schema checking

--- wasp-core/src/main/scala/it/agilelab/bigdata/wasp/core/utils/RowToAvro.scala
+++ wasp-core/src/main/scala/it/agilelab/bigdata/wasp/core/utils/RowToAvro.scala
@@ -1,18 +1,12 @@
 package it.agilelab.bigdata.wasp.core.utils
 
-/**
-  * Created by mattiabertorello on 27/01/17.
-  */
-
-
-
 import java.io.ByteArrayOutputStream
 import java.nio.ByteBuffer
 import java.sql.Timestamp
 import java.util
-import java.util.HashMap
 
 import org.apache.avro.Schema
+import org.apache.avro.Schema.Type
 import org.apache.avro.file.DataFileWriter
 import org.apache.avro.generic.GenericData.Record
 import org.apache.avro.generic.{GenericDatumWriter, GenericRecord}
@@ -20,18 +14,34 @@ import org.apache.spark.sql.Row
 import org.apache.spark.sql.types._
 
 import scala.collection.immutable.Map
+import scala.collection.JavaConverters._
 
+/**
+  * Spark Row to serialized Avro converter class.
+  * The two schemas must be compatible, otherwise an IllegalArgumentExceptio will be thrown at object creation time.
+  * The schemas are compatible if:
+  *   - any field appearing int the Avro schema exists in the Spark schema
+  *   - any such field has the same type in both schemas
+  *   - every field is of type boolean, int, long, float, double, string, complex (StructType for Spark, Record for Avro)
+  *
+  * @author Mattia Bertorello
+  * @author Nicolò Bidotti
+  */
 case class RowToAvro(schema: StructType,
-                schemaAvroJson: String)  {
-
+                     schemaAvroJson: String)  {
+  import RowToAvro._
+  
+  // check schemas are  compatible
+  checkSchemas(schema, schemaAvroJson)
+  
+  // these fields are lazy because otherwise we would not be able to serialize instances of the case class
   private lazy val schemaAvro: Schema = new Schema.Parser().parse(schemaAvroJson)
-
   private lazy val converter: (Any) => Any = createConverterToAvro(schema, schemaAvro)
 
 
   def write(row: Row): Array[Byte] = {
     val output = new ByteArrayOutputStream()
-    val value = converter(row).asInstanceOf[GenericRecord]
+    val value: GenericRecord = converter(row).asInstanceOf[GenericRecord]
     var writer = new DataFileWriter(new GenericDatumWriter[GenericRecord]())
     writer.create(schemaAvro, output)
     writer.append(value)
@@ -112,5 +122,121 @@ case class RowToAvro(schema: StructType,
           }
         }
     }
+  }
+}
+object RowToAvro {
+  private val booleanType = "boolean"
+  private val byteType    = "byte"
+  private val intType     = "int"
+  private val longType    = "long"
+  private val floatType   = "float"
+  private val doubleType  = "double"
+  private val stringType  = "string"
+	
+	/**
+    * Ensures schemas are compatible, that is:
+    *   - any field appearing int the Avro schema exists in the Spark schema
+    *   - any such field has the same type in both schemas
+    */
+  def checkSchemas(schemaSpark: StructType,
+                   schemaAvroJson: String): Unit = {
+    // parse avro schema from json
+    val schemaAvro: Schema = new Schema.Parser().parse(schemaAvroJson)
+    
+    // flatten schemas, convert spark field list into a map
+    val sparkFields: Map[String, String] = flattenSparkSchema(schemaSpark).toMap // the key is the field name, the value is the field type
+    val avroFields: List[(String, String)] = flattenAvroSchema(schemaAvro)
+  
+    // iterate over the Avro field list. If any is missing from the Spark schema, or has a different type, throw an exception.
+    avroFields foreach {
+      case (fieldName, fieldAvroType) => {
+        val maybeFieldSparkType = sparkFields.get(fieldName)
+        if (maybeFieldSparkType.isEmpty) {
+          // field is missing from the Spark schema
+          throw new IllegalArgumentException(s"Field $fieldName in the Avro schema does not exist in the Spark schema.")
+        } else if (maybeFieldSparkType.get != fieldAvroType) {
+          // field has different types in the schemas
+          throw new IllegalArgumentException(s"Field $fieldName has a different type in the schemas. " +
+            s"Type in Avro: $fieldAvroType, type in Spark: ${maybeFieldSparkType.get}")
+        }
+      }
+    }
+  }
+	
+	/**
+    * Flattens the provided Spark schema, returning a list of tuples.
+    * The first element of each tuple is the field name, the second one is the field type.
+    * Nested fields are represented using dot notation.
+    */
+  def flattenSparkSchema(schema: StructType, prefix: String = ""): List[(String, String)] = {
+    schema.fields.toList.flatMap {
+      field => {
+        val name = prefix + field.name
+        val dataType = field.dataType
+        if (dataType.isInstanceOf[StructType]) { // complex type: recurse
+          val prefix = name + "."
+          flattenSparkSchema(dataType.asInstanceOf[StructType], prefix)
+        } else { // simple type: create tuple
+          val tpe = sparkTypeToString(dataType)
+          Seq((name, tpe)) // sequence with a single element because we're in a flatMap
+        }
+      }
+    }
+  }
+  
+  def sparkTypeToString(dataType: DataType): String = dataType match {
+    case _ : BooleanType => booleanType
+    case _ : ByteType    => byteType
+    case _ : IntegerType => intType
+    case _ : LongType    => longType
+    case _ : FloatType   => floatType
+    case _ : DoubleType  => doubleType
+    case _ : StringType  => stringType
+  }
+  
+  /**
+    * Flattens the provided Avro schema, returning a list of tuples.
+    * The first element of each tuple is the field name, the second one is the field type.
+    * Nested fields are represented using dot notation.
+    */
+  def flattenAvroSchema(schema: Schema, prefix: String = ""): List[(String, String)] = {
+    schema.getFields.asScala.toList.flatMap {
+      field => {
+        val name = prefix + field.name
+        val schemaType = field.schema().getType
+        if (schemaType == Type.RECORD) { // complex type: recurse
+          val prefix = name + "."
+          flattenAvroSchema(field.schema, prefix)
+        } else if (schemaType == Type.UNION) { // union type: check that is simple enough, recurse if necessary
+          // drop NullSchema, fail if more than one Schema remains afterwards
+          val nonNullSchemas = field.schema().getTypes.asScala.filter(_.getType != Type.NULL)
+          if (nonNullSchemas.length > 1) {
+            throw new IllegalArgumentException(s"Field $name in the Avro schema has UnionSchema ${field.schema()} which is not a simple NullSchema + primitive schema.")
+          }
+          val remainingSchema = nonNullSchemas.head
+          // recurse if necessary
+          if (remainingSchema.getType == Type.RECORD) { // nullable record, recurse
+            val prefix = name + "."
+            flattenAvroSchema(remainingSchema, prefix)
+          } else { // simple type: create tuple
+            val tpe = avroTypeToString(remainingSchema.getType)
+            Seq((name, tpe)) // sequence with a single element because we're in a flatMap
+          }
+        } else { // simple type: create tuple
+          val tpe = avroTypeToString(schemaType)
+          Seq((name, tpe)) // sequence with a single element because we're in a flatMap
+        }
+      }
+    }
+  }
+  
+  def avroTypeToString(tpe: Type): String = tpe match {
+    case Type.BOOLEAN => booleanType
+    case Type.BYTES   => byteType
+    case Type.INT     => intType
+    case Type.LONG    => longType
+    case Type.FLOAT   => floatType
+    case Type.DOUBLE  => doubleType
+    case Type.STRING  => stringType
   }
 }
